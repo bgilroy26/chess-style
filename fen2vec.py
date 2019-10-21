b82e902a3f7b06d60532ed0283db484fd5d067db
Brendan D. Gilroy <bdgilroy@gmail.com>
2019-10-21 10:24:04 -0400

    fen2vec code from Andreas Stockl

---
+++ fen2vec.py
@@ -0,0 +1,40 @@
+def batchtotensor(inputbatch):
+    
+    pieces_str = "PNBRQK"
+    pieces_str += pieces_str.lower()
+    pieces = set(pieces_str)
+    valid_spaces = set(range(1,9))
+    pieces_dict = {pieces_str[0]:1, pieces_str[1]:2, pieces_str[2]:3, pieces_str[3]:4,
+                    pieces_str[4]:5, pieces_str[5]:6,
+                    pieces_str[6]:-1, pieces_str[7]:-2, pieces_str[8]:-3, pieces_str[9]:-4, 
+                    pieces_str[10]:-5, pieces_str[11]:-6}
+
+    maxnum = len(inputbatch)
+    boardtensor = np.zeros((maxnum, 8, 8,7))
+    
+    for num, inputstr in enumerate(inputbatch):
+        inputliste = inputstr.split()
+        rownr = 0
+        colnr = 0
+        for i, c in enumerate(inputliste[0]):
+            if c in pieces:
+                boardtensor[num, rownr, colnr, np.abs(pieces_dict[c])-1] = np.sign(pieces_dict[c])
+                colnr = colnr + 1
+            elif c == '/':  # new row
+                rownr = rownr + 1
+                colnr = 0
+            elif int(c) in valid_spaces:
+                colnr = colnr + int(c)
+            else:
+                raise ValueError("invalid fenstr at index: {} char: {}".format(i, c))
+        
+        if inputliste[1] == "w":
+            for i in range(8):
+                for j in range(8):
+                    boardtensor[num, i, j, 6] = 1
+        else:
+            for i in range(8):
+                for j in range(8):
+                    boardtensor[num, i, j, 6] = -1
+  
+    return boardtensor
