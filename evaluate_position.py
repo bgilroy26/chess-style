2adcacb55e2108b41ce5959f308fa6edbcffd434
Brendan D. Gilroy <bdgilroy@gmail.com>
2019-10-23 21:50:12 -0400

    figured out ANOVA variable extraction; evaluate position function

---
+++ evaluate_position.py
@@ -0,0 +1,26 @@
+import chess
+import generate_protectors
+import thai_life
+import fen2vec4
+import sys
+import numpy as np
+import pandas as pd
+import torch
+
+#bring in model
+state = torch.load('last_evaluator.tar', map_location=torch.device('cpu'))
+model = thai_life.ThaiLife()
+model.load_state_dict(state['state_dict'])
+model.eval()
+
+#determine MaeToi scores for position
+def get_MaeToi(fen):
+    np_input_vector = fen2vec4.batchtotensor(fen)
+    pytorch_input_vector = torch.from_numpy(np_input_vector).type(torch.FloatTensor)
+    model_output = model(pytorch_input_vector).detach().numpy()[0]
+    return (model_output[0], model_output[1], model_output[2])
+
+if __name__ == '__main__':
+    white_win, draw, black_win = get_MaeToi(sys.argv[1])
+    print(white_win, draw, black_win)
+    
---
+++ fen2vec4.py
@@ -0,0 +1,36 @@
+import numpy as np
+import sys
+
+def batchtotensor(inputbatch):
+    
+    pieces_str = "PNBRQK"
+    pieces_str += pieces_str.lower()
+    pieces = set(pieces_str)
+    valid_spaces = set(range(1,9))
+    pieces_dict = {pieces_str[0]:1, pieces_str[1]:2, pieces_str[2]:3, pieces_str[3]:4,
+                    pieces_str[4]:5, pieces_str[5]:6,
+                    pieces_str[6]:-1, pieces_str[7]:-2, pieces_str[8]:-3, pieces_str[9]:-4, 
+                    pieces_str[10]:-5, pieces_str[11]:-6}
+
+    boardtensor = np.zeros((8, 8,6))
+    
+
+    rownr = 0
+    colnr = 0
+    for i, c in enumerate(inputbatch):
+        if c in pieces:
+            boardtensor[rownr, colnr, np.abs(pieces_dict[c])-1] = np.sign(pieces_dict[c])
+            colnr = colnr + 1
+        elif c == '/':  # new row
+            rownr = rownr + 1
+            colnr = 0
+        elif int(c) in valid_spaces:
+            colnr = colnr + int(c)
+        else:
+            raise ValueError("invalid fenstr at index: {} char: {}".format(i, c))
+        
+  
+    return boardtensor
+
+if __name__ == '__main__':
+   pass 
